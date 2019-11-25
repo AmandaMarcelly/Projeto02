87e19e9b306822f2daced8f1f824bdedaf72f6ee
Amanda Marcelly <amandagrossko@gmail.com>
2019-11-25 19:56:39 -0200

    Final criação Categoria Dao

---
+++ Repository/CategoriaDao.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    class CategoriaDao
+    {
+        private static Context ctx; //= SingletonContext.GetInstance();
+
+        public static Categoria BuscarCategoriaPorNome
+            (Categoria c) =>
+            ctx.Categorias.FirstOrDefault
+            (x => x.Nome.Equals(c.Nome));
+
+        public static List<Categoria> ListarCategoria() => ctx.Categorias.ToList();
+
+        public static bool CadastrarCategoria(Categoria c)
+        {
+            if (BuscarCategoriaPorNome(c) == null)
+            {
+                ctx.Categorias.Add(c);
+                ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static Categoria BuscarCategoriaPorId(int id)
+        {
+            return ctx.Categorias.Find(id);
+        }
+
+    }
+}
